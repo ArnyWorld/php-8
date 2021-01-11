7491523d1bfd5d75847064ba22aead07503567e3
Arnaldo Muñoz <kings31lendy@gmail.com>
2021-01-11 15:23:32 -0400

    Manejo de require e include PHP

---
+++ 17-require.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php';
+//INCLUDE ***  permite añadir ciertos archivos externos, el include se ejecuta n veces 
+//si se le añade n veces si tiene un error se sigue ejecutando 
+// REQUIRE *** solo se añade una vez y si hay un error no se ejecuta 
+//require_once  permite leer un archivo por una única vez
+require 'funciones.php';
+
+iniciarApp();
+
+
+
+include 'includes/footer.php';
